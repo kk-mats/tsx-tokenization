b65df69dc4e870ed4d504d7e81d8a461af23841b
kk-mats <k.mats.kk@gmail.com>
2020-11-07 00:17:45 +0900

    Tokenize samples/1.ts correctly

--- samples/1.ts
+++ samples/1.ts
@@ -3,7 +3,14 @@ namespace ts {
     // If changing the text in this section, be sure to test `configurePrerelease` too.
     export var versionMajorMinor = "4.2";
     /** The version of the TypeScript compiler release */
-    export const version = `${versionMajorMinor}.0-dev`;
+    export const version: TFF<G> = tag`${versionMajorMinor}.0-dev`;
+
+	type TYPE = typeof versionMajorMinor;
+	type OBJECT<T> = {
+		t: T;
+		brand: TYPE;
+	};
+	type U = TYPE extends Array<infer INFER> ? Omit<OBJECT<INFER>, "brand"> : number | string;
 
     /**
      * Type of objects whose values are all of the same type.
@@ -18,20 +25,37 @@ namespace ts {
         " __sortedArrayBrand": any;
     }
 
-    export interface SortedArray<T> extends Array<T> {
-        " __sortedArrayBrand": any;
-    }
-
     /** Common read methods for ES6 Map/Set. */
     export interface ReadonlyCollection<K> {
         readonly size: number;
         has(key: K): boolean;
         keys(): Iterator<K>;
-    }
+	}
 
-    /** Common write methods for ES6 Map/Set. */
-    export interface Collection<K> extends ReadonlyCollection<K> {
-        delete(key: K): boolean;
-        clear(): void;
-    }
+	const ff = (str: string): number => 0;
+
+	async function F() {
+		const i = 0;
+		try {
+			const g = 0;
+			if(g) {
+
+			} else {
+
+			}
+
+		} catch(err) {
+			throw new Error("error");
+		} finally {
+
+		}
+	}
+
+	abstract class CLASS extends PARENT {
+		#de: string;
+
+		private async F(): Promise<void> {
+			return console.log("logger")
+		}
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,94 @@ import * as ts from "typescript";
 
 const scanner = ts.createScanner(ts.ScriptTarget.Latest, true);
 const fileName = path.resolve(__dirname, "../", "samples", "1.ts");
+const sourceText = fs.readFileSync(fileName).toString();
+const sourceFile = ts.createSourceFile(fileName, sourceText, ts.ScriptTarget.Latest);
+
+const tokens: string[] = [];
+const additionalTokens: Record<number, ts.SyntaxKind> = {
+	[ts.SyntaxKind.InterfaceDeclaration]: ts.SyntaxKind.InterfaceKeyword,
+	[ts.SyntaxKind.ReturnStatement]: ts.SyntaxKind.ReturnKeyword,
+	[ts.SyntaxKind.HeritageClause]: ts.SyntaxKind.ExtendsKeyword,
+	[ts.SyntaxKind.NewExpression]: ts.SyntaxKind.NewKeyword,
+	[ts.SyntaxKind.FunctionDeclaration]: ts.SyntaxKind.FunctionKeyword,
+	[ts.SyntaxKind.IfStatement]: ts.SyntaxKind.IfKeyword,
+	[ts.SyntaxKind.ThrowStatement]: ts.SyntaxKind.ThrowKeyword,
+	[ts.SyntaxKind.CatchClause]: ts.SyntaxKind.CatchKeyword,
+	[ts.SyntaxKind.TypeAliasDeclaration]: ts.SyntaxKind.TypeKeyword,
+	[ts.SyntaxKind.TypeQuery]: ts.SyntaxKind.TypeOfKeyword,
+	[ts.SyntaxKind.ConditionalType]: ts.SyntaxKind.ExtendsKeyword,
+	[ts.SyntaxKind.InferType]: ts.SyntaxKind.InferKeyword
+};
+
+const formatSyntaxKind = (kind: ts.SyntaxKind) => ts.SyntaxKind[kind];
+
+const addSyntaxKindToken = (token: ts.SyntaxKind): void => {
+	const tokenString = ts.tokenToString(token);
+	if (tokenString === undefined) {
+		throw new Error(`${formatSyntaxKind(token)} is not a valid token`);
+	}
+	tokens.push(tokenString);
+}
+
+const tokenize = (node: ts.Node) => {
+	const additionalToken = additionalTokens[node.kind];
+	if (additionalToken) {
+		addSyntaxKindToken(additionalToken);
+	}
+
+	const tokenString = ts.tokenToString(node.kind);
+	if (tokenString !== undefined && node.kind!==ts.SyntaxKind.EqualsGreaterThanToken) {
+		tokens.push(tokenString);
+	} else {
+
+		switch (node.kind) {
+			case ts.SyntaxKind.ModuleDeclaration:
+			case ts.SyntaxKind.VariableDeclarationList: {
+				scanner.setText(node.getText(sourceFile));
+				scanner.scan();
+				tokens.push(scanner.getTokenText());
+				break;
+			}
+
+			case ts.SyntaxKind.IfStatement: {
+				const ifNode = node as ts.IfStatement;
+				if (ifNode.elseStatement) {
+					addSyntaxKindToken(ts.SyntaxKind.ElseKeyword);
+				}
+				break;
+			}
+
+			case ts.SyntaxKind.TryStatement: {
+				const tryNode = node as ts.TryStatement;
+				addSyntaxKindToken(ts.SyntaxKind.TryKeyword);
+				if (tryNode.finallyBlock) {
+					addSyntaxKindToken(ts.SyntaxKind.FinallyKeyword);
+				}
+				break;
+			}
+
+			case ts.SyntaxKind.NumericLiteral:
+			case ts.SyntaxKind.Identifier:
+			case ts.SyntaxKind.PrivateIdentifier:
+			case ts.SyntaxKind.StringLiteral:
+			case ts.SyntaxKind.LastTemplateToken: {
+				tokens.push(node.getText(sourceFile));
+				break;
+			}
+
+			default: {
+				// console.log("====>", formatSyntaxKind(node.kind), node.getText(sourceFile));
+			}
+		}
+	}
+
+	ts.forEachChild(node, tokenize);
+};
+
 
 const main = () => {
-	const sourceText = fs.readFileSync(fileName).toString();
-	scanner.setText(sourceText);
-
-	let token = scanner.scan();
-	let insideTemplateExpression = false;
-	let templateExpressionDepth = 0;
-
-	while (token !== ts.SyntaxKind.EndOfFileToken) {
-		if (token === ts.SyntaxKind.Identifier) {
-			console.log(token, "identifier =", JSON.stringify(scanner.getTokenText()));
-		} else if (token === ts.SyntaxKind.NoSubstitutionTemplateLiteral) {
-			insideTemplateExpression=true;
-			++templateExpressionDepth;
-		} else if (token === ts.SyntaxKind.CloseBraceToken) {
-			--templateExpressionDepth;
-			if(templateExpressionDepth===0) {
-				scanner.reScanTemplateToken(false);
-			}
-		} else {
-			console.log(token, scanner.getTokenText());
-		}
-		
-		token = scanner.scan();
-	}
+	ts.forEachChild(sourceFile, tokenize);
+	console.log(tokens);
 };
 
 main();
